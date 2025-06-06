88d46b3cb07b18b7e006fb57c06c3b6b0b0fca42
jnassar <jn.masasin@gmail.com>
2025-06-06 14:59:43 +0200

    Add loitering alarm

---
+++ loitering_alarm.py
@@ -0,0 +1,75 @@
+from lib import LCD, Buzzer, DistanceSensor
+from time import sleep
+
+
+class LoiteringAlarm:
+    RESOLUTION = 0.5  # seconds
+
+    def __init__(
+        self,
+        distance_sensor: DistanceSensor,
+        display: LCD,
+        buzzer: Buzzer,
+        *,
+        min_distance_cm: float = 60,
+        max_distance_cm: float = 120,
+        alert_after_seconds: int = 5 * 60,
+        timeout_seconds: int = 30,
+    ) -> None:
+        self.distance_sensor = distance_sensor
+        self.display = display
+        self.buzzer = buzzer
+        self.min_distance_cm = min_distance_cm
+        self.max_distance_cm = max_distance_cm
+        self.alert_after_seconds = alert_after_seconds
+        self.timeout_seconds = timeout_seconds
+
+        self._elapsed_time = 0
+        self._occluded_time = 0
+
+    def run(self):
+        while True:
+            if (distance := self.distance_sensor.distance) is None:
+                continue
+
+            self._write_distance(distance)
+            self._write_elapsed_time()
+
+            self._update_times(distance)
+
+            self._trigger_buzzer()
+
+            sleep(self.RESOLUTION)
+
+    def _write_distance(self, distance: float) -> None:
+        self.display.set_cursor(0, 0)
+        self.display.write(f"{distance:5.1f} cm")
+
+    def _write_elapsed_time(self) -> None:
+        countdown = max(0, self.alert_after_seconds - self._elapsed_time)
+        minutes, seconds = divmod(countdown, 60)
+
+        self.display.set_cursor(1, 0)
+        self.display.write(f"{minutes:02.0f}:{seconds:02.0f}")
+
+    def _update_times(self, distance: float) -> None:
+        if self.min_distance_cm <= distance <= self.max_distance_cm:
+            self._elapsed_time += self.RESOLUTION
+            return
+
+        if self._elapsed_time == 0:
+            return
+
+        self._elapsed_time += self.RESOLUTION
+        self._occluded_time += self.RESOLUTION
+        if self._occluded_time >= self.timeout_seconds:
+            self._elapsed_time = 0
+            self._occluded_time = 0
+
+    def _trigger_buzzer(self) -> None:
+        if self._elapsed_time < self.alert_after_seconds:
+            if self.buzzer.is_on:
+                self.buzzer.off()
+            return
+
+        self.buzzer.on()
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from time import sleep
+from loitering_alarm import LoiteringAlarm
 
-from lib import HC_SR04, AE_AQM0802
+from lib import (
+    AE_AQM0802,
+    Buzzer,
+    HC_SR04,
+)
 
 
 distance_sensor = HC_SR04(trigger_pin=14, echo_pin=15)
 display = AE_AQM0802(clock_pin=17, data_pin=16)
+buzzer = Buzzer(pin_number=13, is_active=False)
 
-while True:
-    if (distance := distance_sensor.distance) is not None:
-        print(f"Distance: {distance:.1f} cm")
-        display.write(f"{distance:.1f} cm" + " " * 8)
-        display.set_cursor(0, 0)
-    sleep(0.5)
+loitering_alarm = LoiteringAlarm(
+    distance_sensor=distance_sensor,
+    display=display,
+    buzzer=buzzer,
+    min_distance_cm=10,
+    max_distance_cm=20,
+    alert_after_seconds=10,
+    timeout_seconds=5,
+)
+
+loitering_alarm.run()
