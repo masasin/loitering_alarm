59981a1cb49cde5c88ddaa88500051d71fb5e03f
jnassar <jn.masasin@gmail.com>
2025-06-06 15:18:59 +0200

    Add ifname

--- main.py
+++ main.py
@@ -7,18 +7,40 @@ from lib import (
 )
 
 
-distance_sensor = HC_SR04(trigger_pin=14, echo_pin=15)
-display = AE_AQM0802(clock_pin=17, data_pin=16)
-buzzer = Buzzer(pin_number=13, is_active=False)
+def build_test_loitering_alarm(*, distance_sensor, display, buzzer):
+    return LoiteringAlarm(
+        distance_sensor=distance_sensor,
+        display=display,
+        buzzer=buzzer,
+        min_distance_cm=10,
+        max_distance_cm=20,
+        alert_after_seconds=10,
+        timeout_seconds=5,
+    )
 
-loitering_alarm = LoiteringAlarm(
-    distance_sensor=distance_sensor,
-    display=display,
-    buzzer=buzzer,
-    min_distance_cm=10,
-    max_distance_cm=20,
-    alert_after_seconds=10,
-    timeout_seconds=5,
-)
 
-loitering_alarm.run()
+def build_loitering_alarm(*, distance_sensor, display, buzzer):
+    return LoiteringAlarm(
+        distance_sensor=distance_sensor,
+        display=display,
+        buzzer=buzzer,
+        min_distance_cm=60,
+        max_distance_cm=120,
+        alert_after_seconds=5 * 60,
+        timeout_seconds=30,
+    )
+
+
+if __name__ == "__main__":
+    distance_sensor = HC_SR04(trigger_pin=14, echo_pin=15)
+    display = AE_AQM0802(clock_pin=17, data_pin=16)
+    buzzer = Buzzer(pin_number=13, is_active=False)
+
+    # loitering_alarm = build_test_loitering_alarm(
+    loitering_alarm = build_loitering_alarm(
+        distance_sensor=distance_sensor,
+        display=display,
+        buzzer=buzzer,
+    )
+
+    loitering_alarm.run()
