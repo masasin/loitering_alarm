8169b6d0b85f272c4b86075907d7716601b8517a
Jean Nassar <jean@jnassar.com>
2025-07-21 21:30:57 +0200

    Remove spurious scans

--- lib/tools.py
+++ lib/tools.py
@@ -5,7 +5,6 @@ from lib.utils import Pin
 
 def scan_i2c_devices(clock_pin: int, data_pin: int) -> None:
     i2c = machine.I2C(0, scl=Pin(clock_pin), sda=Pin(data_pin), freq=400_000)
-    devices = i2c.scan()
 
     print("Scanning I2C addresses...")
 
@@ -14,5 +13,5 @@ def scan_i2c_devices(clock_pin: int, data_pin: int) -> None:
     if not devices:
         print("No I2C devices found.")
 
-    for device in i2c.scan():
+    for device in devices:
         print(f"Found I2C device at address: {hex(device)}")
